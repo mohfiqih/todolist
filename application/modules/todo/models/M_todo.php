b8b2cd635821260beb37ea3f9604eb4fd828521b
wirayuda <yudaardi@gmail.com>
2022-01-05 22:28:45 +0700

    .

--- application/modules/todo/models/M_todo.php
+++ application/modules/todo/models/M_todo.php
@@ -5,10 +5,10 @@ class M_todo extends CI_Model
 {
     public function get_todo($id_todo = null) 
     {
-     $this->db->select('todo.id as id_todo, todo.id_user, todo.task, todo.mulai, todo.selesai, todo.date_created, todo.date_updated, todo.level, todo.status, todo.checked');
+     $this->db->select('*');
      $this->db->from('todo');
      $this->db->join('user', 'user.user_id = todo.id_user');
-     if($id_todo) $this->db->where('todo.id',$id_todo);
+    //  if($id_todo) $this->db->where('todo.id',$id_todo);
      $query = $this->db->get()->result();
      return $query;
     }
