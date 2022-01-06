c06b6d136d085467decf4380cb7df4073bc452bc
wirayuda <yudaardi@gmail.com>
2022-01-06 09:10:38 +0700

    .

--- application/modules/todo/models/M_todo.php
+++ application/modules/todo/models/M_todo.php
@@ -3,19 +3,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_todo extends CI_Model
 {
-    public function get_todo($id_todo = null) 
+    public function get_todo() 
     {
      $this->db->select('*');
      $this->db->from('todo');
      $this->db->join('user', 'user.user_id = todo.id_user');
-    //  if($id_todo) $this->db->where('todo.id',$id_todo);
      $query = $this->db->get()->result();
      return $query;
     }
 
-    public function get_relasi()
-    {
-        # code...
-    }
+    
 }
 ?>
