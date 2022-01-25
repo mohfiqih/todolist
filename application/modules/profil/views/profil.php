fa74bc500c5135ff58bd3f12a7f9e69fba73f13a
moh fiqih <mfiqiherinsyah90@gmail.com>
2022-01-25 09:23:38 +0700

    push

--- application/modules/profil/views/profil.php
+++ application/modules/profil/views/profil.php
@@ -3,35 +3,50 @@
 
           <!-- Start Content-->
           <div class="container-fluid">
-               <div class="card">
-                    <div class="card-body">
-                         <form action="<?php echo base_url('profil/update'); ?>" method="POST"
-                              enctype="multipart/form-data">
-                              <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>"
-                                   value="<?php echo $this->security->get_csrf_hash(); ?>">
+               <form action="<?php echo base_url('profil/update'); ?>" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>"
+                         value="<?php echo $this->security->get_csrf_hash(); ?>">
 
-                              <div class="row push">
-                                   <div class="col-lg-4"></div>
-                                   <div class="col-lg-4">
-                                        <div class="mb-4">
+                    <div class="row">
+                         <div class="col-md-4">
+                              <div class="card">
+                                   <div class="card-body">
+
+                                        <center>
+                                             <img style="width: 210px; height: 210px;" type="file" alt="poltek"
+                                                  src="<?php echo base_url('assets/backend'); ?>/images/bg-auth3.png" /><br />
+                                        </center>
+                                        <br />
+                                        <div class="mb-2"><br />
+                                             <label class="form-label">Foto Profil</label>
+                                             <input type="file" class="form-control" name="foto" value=""><br />
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="col-md-8">
+                              <div class="card">
+                                   <div class="card-body">
+                                        <div class="mb-2">
                                              <label class="form-label">Nama Lengkap</label>
                                              <input type="text" class="form-control" name="nama_lengkap" value="">
                                         </div>
-                                        <div class="mt-5 mb-4">
+                                        <div class="mb-2">
                                              <label class="form-label">Password Saat Ini</label>
                                              <input type="password" class="form-control" name="password_sekarang">
                                         </div>
-                                        <div class="row mb-4">
-                                             <div class="col-12">
-                                                  <label class="form-label">Password Baru</label>
-                                                  <input type="password" class="form-control" name="password_baru_1">
-                                             </div>
+                                        <div class="mb-2">
+
+                                             <label class="form-label">Password Baru</label>
+                                             <input type="password" class="form-control" name="password_baru_1">
+
                                         </div>
-                                        <div class="row mb-4">
-                                             <div class="col-12">
-                                                  <label class="form-label">Ulangi Password Baru</label>
-                                                  <input type="password" class="form-control" name="password_baru_2">
-                                             </div>
+                                        <div class="mb-2">
+
+                                             <label class="form-label">Ulangi Password Baru</label>
+                                             <input type="password" class="form-control" name="password_baru_2">
+
                                         </div>
                                         <div class="text-center">
                                              <button type="submit" class="btn btn-primary">Update</button>
@@ -41,9 +56,9 @@
                                         </div>
                                    </div>
                               </div>
-                         </form>
-                    </div>
-               </div>
+               </form>
           </div>
      </div>
+</div>
+</div>
 </div>
