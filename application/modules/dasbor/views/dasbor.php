1bc22ad6f0cf541db4e949b09deaf2c08e8aabfe
moh fiqih <mfiqiherinsyah90@gmail.com>
2022-01-30 21:57:29 +0700

    kirim

--- application/modules/dasbor/views/dasbor.php
+++ application/modules/dasbor/views/dasbor.php
@@ -428,41 +428,6 @@
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-end">
                                              <!-- item-->
-                                             <a href="<?php echo base_url('user'); ?>" class="dropdown-item">More
-                                                  Info</a>
-
-                                        </div>
-                                   </div>
-
-                                   <h4 class="header-title mt-0 mb-4">Total User</h4>
-
-                                   <div class="widget-chart-1">
-                                        <div class="widget-chart-box-1 float-start" dir="ltr">
-                                             <input data-plugin="knob" data-width="70" data-height="70"
-                                                  data-fgColor="#f05050 " data-bgColor="#F9B9B9" value="58"
-                                                  data-skin="tron" data-angleOffset="180" data-readOnly=true
-                                                  data-thickness=".15" />
-                                        </div>
-
-                                        <div class="widget-detail-1 text-end">
-                                             <h2 class="fw-normal pt-2 mb-1"> <?php  echo $jml_user; ?> </h2>
-                                             <p class="text-muted mb-1">Users</p>
-                                        </div>
-                                   </div>
-                              </div>
-                         </div>
-                    </div><!-- end col -->
-
-                    <div class="col-xl-3 col-md-6">
-                         <div class="card">
-                              <div class="card-body">
-                                   <div class="dropdown float-end">
-                                        <a href="#" class="dropdown-toggle arrow-none card-drop"
-                                             data-bs-toggle="dropdown" aria-expanded="false">
-                                             <i class="mdi mdi-dots-vertical"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-end">
-                                             <!-- item-->
                                              <a href="<?php echo base_url('todo'); ?>" class="dropdown-item">More
                                                   Info</a>
 
